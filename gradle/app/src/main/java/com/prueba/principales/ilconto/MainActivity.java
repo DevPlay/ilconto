16ca7ab703a567c7ae14871150270e3cd44c6cbf
nicahdemarco <mortalfombra@gmail.com>
2016-11-22 20:38:38 -0300

    que se yo, cambie varias cosas pero no funciona todavia

---
+++ gradle/app/src/main/java/com/prueba/principales/ilconto/MainActivity.java
@@ -0,0 +1,228 @@
+package com.prueba.principales.ilconto;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+    private EditText saldo,comensales, propina;
+    private DatabaseReference mDatabase;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                submitPost();
+                Toast.makeText(getApplicationContext(), "Se solicita al mozo en la mesa: ", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private void submitPost() {
+
+        String key = mDatabase.child("posts").push().getKey();
+        Map<String, Object> postValues = new HashMap<>();
+        postValues.put("Accion", "Llamar al mozo");
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/posts/" + key, postValues);
+
+        mDatabase.updateChildren(childUpdates);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void suma (View view) {
+        saldo = (EditText) this.findViewById(R.id.saldo1);
+        comensales = (EditText) this.findViewById(R.id.comensales2);
+        propina = (EditText) this.findViewById(R.id.propina2);
+        double n1 = Double.parseDouble(saldo.getText().toString());
+        double n2 = Double.parseDouble(comensales.getText().toString());
+        double n3;
+        double n4;
+        double resultado = 0;
+        if (propina == null ) {
+            n4 = 1;
+            resultado = (n1/n2)*n4;
+        } else {
+            n3 = Double.parseDouble(propina.getText().toString());
+            n4 = (n3/100)+1;
+            resultado = (n1/n2)*n4;
+        }
+
+        DecimalFormat dec = new DecimalFormat("#.##");
+        String resultado1 = dec.format(resultado);
+        String r_final1 = String.valueOf(resultado1);
+        Toast.makeText(getApplicationContext(), "Son: $" + r_final1 + " por persona" , Toast.LENGTH_LONG).show();
+    }
+
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
+
+
+                View rootView = inflater.inflate(R.layout.fragment_calculadora, container, false);
+
+                return rootView;
+            }
+            if (getArguments().getInt(ARG_SECTION_NUMBER) == 2) {
+
+
+                View rootView = inflater.inflate(R.layout.fragment_opiniones, container, false);
+                return rootView;
+            }
+            else {
+
+
+                View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+                textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+                return rootView;
+            }
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+            public SectionsPagerAdapter(FragmentManager fm) {
+                super(fm);
+            }
+
+            @Override
+            public Fragment getItem(int position) {
+                // getItem is called to instantiate the fragment for the given page.
+                // Return a PlaceholderFragment (defined as a static inner class below).
+                return PlaceholderFragment.newInstance(position + 1);
+            }
+
+            @Override
+            public int getCount() {
+                // Show 3 total pages.
+                return 3;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position) {
+                    case 0:
+                        return "Calculadora";
+                    case 1:
+                        return "Opiniones";
+                    case 2:
+                        return "Estas en...";
+                }
+                return null;
+            }
+        }
+    }
